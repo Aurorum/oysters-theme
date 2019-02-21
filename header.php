a77237e0fae425c1b89936e8075047a7b41f7a29
torres126 <43215253+torres126@users.noreply.github.com>
2019-02-21 17:00:47 +0000

    Commit Moana Theme 🍃

---
+++ header.php
@@ -0,0 +1,130 @@
+<?php
+/**
+ * The Header for Moana
+ *
+ * Displays all of the <head> section and everything up till <div id="content">
+ *
+ * @package Moana
+ */
+?><!DOCTYPE html>
+
+<html <?php
+language_attributes();
+?>>
+
+<head>
+
+<meta charset="<?php
+bloginfo('charset');
+?>">
+
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link href="https://fonts.googleapis.com/css?family=Roboto|Titillium+Web" rel="stylesheet">
+<link rel="profile" href="http://gmpg.org/xfn/11">
+
+<?php
+wp_head();
+?>
+
+</head>
+
+<body <?php
+body_class();
+?>>
+
+<div id="page" class="hfeed site">
+
+
+    <a class="skip-link screen-reader-text" href="#content"><?php
+esc_html_e('Skip to content', 'moana');
+?></a>    <header id="masthead" class="site-header" role="banner"> <?php
+the_custom_logo();
+if (is_front_page() && is_home()):
+?>
+            <?php
+else:
+?>
+	             <?php
+endif;
+?>
+
+	<?php if ( function_exists( 'jetpack_social_menu' ) ) jetpack_social_menu();?>
+
+    </header><!-- #masthead -->
+	
+        <div class="site-branding">
+<?php
+$description = get_bloginfo('description', 'display');
+if ($description || is_customize_preview()):
+?>
+               <p class="site-description"><?php
+    echo $description;
+    /* WPCS: xss ok. */
+?></p>
+
+            <?php
+endif;
+?>
+                        <?php
+if (!is_front_page() && !is_home()):
+?>
+
+
+        <h1 class="secondary-title"><a href="<?php
+    echo esc_url(home_url('/'));
+?>" rel="home"><?php
+    bloginfo('name');
+?></a></h1>
+
+            <?php
+endif;
+?>
+
+        </div>
+
+        <nav id="site-navigation" class="main-navigation" role="navigation">
+            
+            <?php
+wp_nav_menu(array(
+    'theme_location' => 'menu-1'
+));
+?>
+
+        </nav><!-- #site-navigation -->
+
+                <h1 class="site-title"><a href="<?php
+    echo esc_url(home_url('/'));
+?>" rel="home"><?php
+    bloginfo('name');
+?></a></h1>
+
+                <p class="site-title"><a href="<?php
+    echo esc_url(home_url('/'));
+?>" rel="home"><?php
+    bloginfo('name');
+?></a></p>
+
+
+    <div id="content" class="site-content">
+
+        <?php
+if (get_header_image()):
+?>
+
+            <a href="<?php
+    echo esc_url(home_url('/'));
+?>" rel="home">
+
+                <img class="custom-header" src="<?php
+    header_image();
+?>" width="<?php
+    echo get_custom_header()->width;
+?>" height="<?php
+    echo get_custom_header()->height;
+?>" alt="">
+
+            </a>
+
+             <?php
+endif;
+?>
---
+++ readme.txt
@@ -0,0 +1,52 @@
+=== Summer ===
+
+Contributors: torres126
+Tags: blog, one-column, two-columns, right-sidebar, custom-background, custom-colors, featured-images, custom-header, custom-logo, custom-menu, sticky-post, threaded-comments, translation-ready, block-styles
+Requires at least: 4.5
+Tested up to: 5.0.3
+Stable tag: 1.0.0
+License: GNU General Public License v2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+== Description ==
+
+Moana is a fresh, clean theme designed to make your blog vibrant and lively, with minimal but effective styling combined with georgeous colours, allowing your content to truly be breathaking. 
+
+== Installation ==
+
+1. In your admin panel, go to Appearance > Themes and click the Add New button.
+2. Click Upload Theme and Choose File, then select the theme's .zip file. Click Install Now.
+3. Click Activate to use your new theme right away.
+
+== Frequently Asked Questions ==
+
+== How do I set up my theme? ==
+
+Moana's demo can be replicated in a matter of minutes, all through the Customizer, with just five simple steps:
+
+1. Set your Header Image
+2. Visit the "Colors" tab and pick the header and primary colour for your site
+3. From "Site Identity", upload your custom logo
+4. If you have the Jetpack plugin installed, select "Menus" and add your Social Menu, with links to all your social networks
+5. Lastly, head to the Widgets section and add your favourite widgets to trigger your sidebar 
+
+From there, all you need to do is write your first post! 
+
+= Does this theme support any plugins? =
+
+Moana includes support for Jetpack's Infinite Scroll, Social Menus and other benefits it offers, such as Responsive Videos.
+
+= There's something which I don't like! =
+
+Moana welcomes and encourages all feedback and any bug reports for future updates! In the meantime, custom CSS can be provided to ensure your blog looks just how you'd like it.
+
+== Changelog ==
+
+= 1.0 - Feb 21 2019 =
+* Initial release
+
+== Credits ==
+
+* Based on Underscores https://underscores.me/, (C) 2012-2019 Automattic, Inc., [GPLv2 or later](https://www.gnu.org/licenses/gpl-2.0.html)
+* Site logo in screenshot, Club Penguin
+* normalize.css https://necolas.github.io/normalize.css/, (C) 2012-2019 Nicolas Gallagher and Jonathan Neal, [MIT](https://opensource.org/licenses/MIT)
