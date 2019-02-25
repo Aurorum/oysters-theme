d35efde2c1a399c65db6e4331811bc52eb96bcb2
torres126 <43215253+torres126@users.noreply.github.com>
2019-02-25 20:15:38 +0000

    Rename

--- inc/customizer.php
+++ inc/customizer.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * Moana Theme Customizer
+ * Oyster Theme Customizer
  *
- * @package Moana
+ * @package Oyster
  */
 
 /**
@@ -18,11 +18,11 @@ function moana_customize_register( $wp_customize ) {
 	if ( isset( $wp_customize->selective_refresh ) ) {
 		$wp_customize->selective_refresh->add_partial( 'blogname', array(
 			'selector'        => '.site-title a',
-			'render_callback' => 'moana_customize_partial_blogname',
+			'render_callback' => 'oyster_customize_partial_blogname',
 		) );
 		$wp_customize->selective_refresh->add_partial( 'blogdescription', array(
 			'selector'        => '.site-description',
-			'render_callback' => 'moana_customize_partial_blogdescription',
+			'render_callback' => 'oyster_customize_partial_blogdescription',
 		) );
 	}
 }
@@ -33,7 +33,7 @@ add_action( 'customize_register', 'moana_customize_register' );
  *
  * @return void
  */
-function moana_customize_partial_blogname() {
+function oyster_customize_partial_blogname() {
 	bloginfo( 'name' );
 }
 
@@ -42,14 +42,14 @@ function moana_customize_partial_blogname() {
  *
  * @return void
  */
-function moana_customize_partial_blogdescription() {
+function oyster_customize_partial_blogdescription() {
 	bloginfo( 'description' );
 }
 
 /**
  * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
  */
-function moana_customize_preview_js() {
+function oyster_customize_preview_js() {
 	wp_enqueue_script( 'moana-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
 }
-add_action( 'customize_preview_init', 'moana_customize_preview_js' );
+add_action( 'customize_preview_init', 'oyster_customize_preview_js' );
