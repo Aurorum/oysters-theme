005aaa43750f9a67ad43037edafae39b1e1fa6e0
torres126 <43215253+torres126@users.noreply.github.com>
2019-02-25 21:03:19 +0000

    Update template-tags.php

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -4,10 +4,10 @@
  *
  * Eventually, some of the functionality here could be replaced by core features.
  *
- * @package Moana
+ * @package Oyster
  */
 
-if ( ! function_exists( 'moana_posted_on' ) ) :
+if ( ! function_exists( 'oyster_posted_on' ) ) :
 	/**
 	 * Prints HTML with meta information for the current post-date/time.
 	 */
@@ -39,7 +39,7 @@ if ( ! function_exists( 'moana_posted_by' ) ) :
 	/**
 	 * Prints HTML with meta information for the current author.
 	 */
-	function moana_posted_by() {
+	function oyster_posted_by() {
 		$byline = sprintf(
 			/* translators: %s: post author. */
 			esc_html_x( 'by %s', 'post author', 'moana' ),
@@ -55,7 +55,7 @@ if ( ! function_exists( 'moana_entry_footer' ) ) :
 	/**
 	 * Prints HTML with meta information for the categories, tags and comments.
 	 */
-	function moana_entry_footer() {
+	function oyster_entry_footer() {
 		// Hide category and tag text for pages.
 		if ( 'post' === get_post_type() ) {
 			/* translators: used between list items, there is a space after the comma */
@@ -96,7 +96,7 @@ if ( ! function_exists( 'moana_entry_footer' ) ) :
 			sprintf(
 				wp_kses(
 					/* translators: %s: Name of current post. Only visible to screen readers */
-					__( 'Edit <span class="screen-reader-text">%s</span>', 'moana' ),
+					__( 'Edit <span class="screen-reader-text">%s</span>', 'oyster' ),
 					array(
 						'span' => array(
 							'class' => array(),
@@ -111,14 +111,14 @@ if ( ! function_exists( 'moana_entry_footer' ) ) :
 	}
 endif;
 
-if ( ! function_exists( 'moana_post_thumbnail' ) ) :
+if ( ! function_exists( 'oyster_post_thumbnail' ) ) :
 	/**
 	 * Displays an optional post thumbnail.
 	 *
 	 * Wraps the post thumbnail in an anchor element on index views, or a div
 	 * element when on single views.
 	 */
-	function moana_post_thumbnail() {
+	function oyster_post_thumbnail() {
 		if ( post_password_required() || is_attachment() || ! has_post_thumbnail() ) {
 			return;
 		}
