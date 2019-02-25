6f39f07deff78566a5c293b730ea3f935789a6c0
torres126 <43215253+torres126@users.noreply.github.com>
2019-02-25 22:10:41 +0000

    Commit Oyster Theme 🐚

--- template-parts/content-search.php
+++ template-parts/content-search.php
@@ -16,20 +16,20 @@
 		<?php if ( 'post' === get_post_type() ) : ?>
 		<div class="entry-meta">
 			<?php
-			moana_posted_on();
-			moana_posted_by();
+			oyster_posted_on();
+			oyster_posted_by();
 			?>
 		</div><!-- .entry-meta -->
 		<?php endif; ?>
 	</header><!-- .entry-header -->
 
-	<?php moana_post_thumbnail(); ?>
+	<?php oyster_post_thumbnail(); ?>
 
 	<div class="entry-summary">
 		<?php the_excerpt(); ?>
 	</div><!-- .entry-summary -->
 
 	<footer class="entry-footer">
-		<?php moana_entry_footer(); ?>
+		<?php oyster_entry_footer(); ?>
 	</footer><!-- .entry-footer -->
 </article><!-- #post-<?php the_ID(); ?> -->
--- template-parts/content.php
+++ template-parts/content.php
@@ -22,21 +22,21 @@
 			?>
 			<div class="entry-meta">
 				<?php
-				moana_posted_on();
-				moana_posted_by();
+				oyster_posted_on();
+				oyster_posted_by();
 				?>
 			</div><!-- .entry-meta -->
 		<?php endif; ?>
 	</header><!-- .entry-header -->
 
-	<?php moana_post_thumbnail(); ?>
+	<?php oyster_post_thumbnail(); ?>
 
 	<div class="entry-content">
 		<?php
 		the_content( sprintf(
 			wp_kses(
 				/* translators: %s: Name of current post. Only visible to screen readers */
-				__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'moana' ),
+				__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'oyster' ),
 				array(
 					'span' => array(
 						'class' => array(),
@@ -47,13 +47,13 @@
 		) );
 
 		wp_link_pages( array(
-			'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'moana' ),
+			'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'oyster' ),
 			'after'  => '</div>',
 		) );
 		?>
 	</div><!-- .entry-content -->
 
 	<footer class="entry-footer">
-		<?php moana_entry_footer(); ?>
+		<?php oyster_entry_footer(); ?>
 	</footer><!-- .entry-footer -->
 </article><!-- #post-<?php the_ID(); ?> -->
