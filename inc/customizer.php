f292df1fc91d79ee94bbbbe83288a931eb1afa7a
torres126 <43215253+torres126@users.noreply.github.com>
2019-02-25 20:17:03 +0000

    Update customizer.php

--- inc/customizer.php
+++ inc/customizer.php
@@ -10,7 +10,7 @@
  *
  * @param WP_Customize_Manager $wp_customize Theme Customizer object.
  */
-function moana_customize_register( $wp_customize ) {
+function oyster_customize_register( $wp_customize ) {
 	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
 	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
 	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
@@ -50,6 +50,6 @@ function oyster_customize_partial_blogdescription() {
  * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
  */
 function oyster_customize_preview_js() {
-	wp_enqueue_script( 'moana-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
+	wp_enqueue_script( 'oyster-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
 }
 add_action( 'customize_preview_init', 'oyster_customize_preview_js' );
