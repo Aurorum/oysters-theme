ed7a34d15d77553b7e190044db07b2db8f0dcb99
torres126 <43215253+torres126@users.noreply.github.com>
2019-03-05 22:33:50 +0000

    Fix a fatal

--- inc/customizer.php
+++ inc/customizer.php
@@ -10,7 +10,7 @@
  *
  * @param WP_Customize_Manager $wp_customize Theme Customizer object.
  */
-function oyster_customize_register( $wp_customize ) {
+function oysters_customize_register( $wp_customize ) {
 	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
 	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
 	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
