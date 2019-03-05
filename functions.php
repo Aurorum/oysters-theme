f0f609de284f5f87b5cd282a2773a4828962fba9
torres126 <43215253+torres126@users.noreply.github.com>
2019-03-05 16:53:43 +0000

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,13 +1,13 @@
 <?php
 /**
- * Oyster functions and definitions
+ * Oysters functions and definitions
  *
  * @link https://developer.wordpress.org/themes/basics/theme-functions/
  *
- * @package Oyster
+ * @package Oysters
  */
 
-if ( ! function_exists( 'oyster_setup' ) ) :
+if ( ! function_exists( 'oysters_setup' ) ) :
 	/**
 	 * Sets up theme defaults and registers support for various WordPress features.
 	 *
@@ -15,12 +15,12 @@ if ( ! function_exists( 'oyster_setup' ) ) :
 	 * runs before the init hook. The init hook is too late for some features, such
 	 * as indicating support for post thumbnails.
 	 */
-	function oyster_setup() {
+	function oysters_setup() {
 		/*
 		 * Make theme available for translation.
 		 * Translations can be filed in the /languages/ directory.
 		 */
-		load_theme_textdomain( 'oyster', get_template_directory() . '/languages' );
+		load_theme_textdomain( 'oysters', get_template_directory() . '/languages' );
 
 		// Add default posts and comments RSS feed links to head.
 		add_theme_support( 'automatic-feed-links' );
@@ -42,7 +42,7 @@ if ( ! function_exists( 'oyster_setup' ) ) :
 
 		// This theme uses wp_nav_menu() in one location.
 		register_nav_menus( array(
-			'menu-1' => esc_html__( 'Primary', 'oyster' ),
+			'menu-1' => esc_html__( 'Primary', 'oysters' ),
 		) );
 
 		/*
@@ -58,7 +58,7 @@ if ( ! function_exists( 'oyster_setup' ) ) :
 		) );
 
 		// Set up the WordPress core custom background feature.
-		add_theme_support( 'custom-background', apply_filters( 'oyster_custom_background_args', array(
+		add_theme_support( 'custom-background', apply_filters( 'oysters_custom_background_args', array(
 			'default-color' => 'ffffff',
 			'default-image' => '',
 		) ) );
@@ -82,7 +82,7 @@ if ( ! function_exists( 'oyster_setup' ) ) :
 		) );
 	}
 endif;
-add_action( 'after_setup_theme', 'oyster_setup' );
+add_action( 'after_setup_theme', 'oysters_setup' );
 
 /**
  * Set the content width in pixels, based on the theme's design and stylesheet.
@@ -91,7 +91,7 @@ add_action( 'after_setup_theme', 'oyster_setup' );
  *
  * @global int $content_width
  */
-function oyster_content_width() {
+function oysters_content_width() {
 	// This variable is intended to be overruled from themes.
 	// Open WPCS issue: {@link https://github.com/WordPress-Coding-Standards/WordPress-Coding-Standards/issues/1043}.
 	// phpcs:ignore WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedVariableFound
@@ -104,40 +104,40 @@ add_action( 'after_setup_theme', 'oyster_content_width', 0 );
  *
  * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
  */
-function oyster_widgets_init() {
+function oysters_widgets_init() {
 	register_sidebar( array(
-		'name'          => esc_html__( 'Sidebar', 'oyster' ),
+		'name'          => esc_html__( 'Sidebar', 'oysters' ),
 		'id'            => 'sidebar-1',
-		'description'   => esc_html__( 'Add widgets here.', 'oyster' ),
+		'description'   => esc_html__( 'Add widgets here.', 'oysters' ),
 		'before_widget' => '<section id="%1$s" class="widget %2$s">',
 		'after_widget'  => '</section>',
 		'before_title'  => '<h2 class="widget-title">',
 		'after_title'   => '</h2>',
 	) );
 }
-add_action( 'widgets_init', 'oyster_widgets_init' );
+add_action( 'widgets_init', 'oysters_widgets_init' );
 
 /**
  * Enqueue scripts and styles.
  */
-function oyster_scripts() {
-	wp_enqueue_style( 'oyster-style', get_stylesheet_uri() );
+function oysters_scripts() {
+	wp_enqueue_style( 'oysters-style', get_stylesheet_uri() );
 	
-	wp_enqueue_style( 'oyster-fonts', '//fonts.googleapis.com/css?family=Roboto|Titillium+Web');
+	wp_enqueue_style( 'oysters-fonts', '//fonts.googleapis.com/css?family=Roboto|Titillium+Web');
 
-	wp_enqueue_script( 'oyster-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
+	wp_enqueue_script( 'oysters-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
 
-	wp_enqueue_script( 'oyster-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+	wp_enqueue_script( 'oysters-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
 	
 	/* Gutenberg */
-	wp_enqueue_style( 'oyster-blocks', get_template_directory_uri() . '/blocks.css' );
-	wp_enqueue_style( 'oyster-editor-block-style', get_template_directory_uri() . '/editor-blocks.css');
+	wp_enqueue_style( 'oysters-blocks', get_template_directory_uri() . '/blocks.css' );
+	wp_enqueue_style( 'oysters-editor-block-style', get_template_directory_uri() . '/editor-blocks.css');
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
 }
-add_action( 'wp_enqueue_scripts', 'oyster_scripts' );
+add_action( 'wp_enqueue_scripts', 'oysters_scripts' );
 
 /**
  * Implement the Custom Header feature.
